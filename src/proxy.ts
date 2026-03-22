bdcbc68b43c6df783218fa59e79b73171afb8ef6
Sanidhya Tilekar <tilekarsanidhya@gmail.com>
2026-03-22 20:10:20 +0530

    Update proxy.ts

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,7 +1,9 @@
 import NextAuth from "next-auth"
 import { authConfig } from "./auth.config"
 
-export const { auth} = NextAuth(authConfig)
+const { auth } = NextAuth(authConfig)
+
+export const proxy = auth
 
 export const config = {
   matcher: ["/dashboard/:path*", "/login"],
