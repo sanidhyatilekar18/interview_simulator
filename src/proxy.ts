098fc268cbc4ed74851c2ebc8402cf11f36dd703
Sanidhya Tilekar <tilekarsanidhya@gmail.com>
2026-03-22 20:03:19 +0530

    .

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth"
 import { authConfig } from "./auth.config"
 
-export const { auth: middleware } = NextAuth(authConfig)
+export const { auth} = NextAuth(authConfig)
 
 export const config = {
   matcher: ["/dashboard/:path*", "/login"],
